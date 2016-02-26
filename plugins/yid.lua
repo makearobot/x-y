7ecdb96eb873cbaaa2bf942ab8899761838a445f
makearobot <mrvigeo@chmail.ir>
2016-02-26 07:28:07 -0800

    MrVigeo

---
+++ plugins/yid.lua
@@ -0,0 +1,18 @@
+
+do
+   local function add_user(chat, user)
+   local status = chat_add_user (chat, user, ok_cb, false)
+   if not status then
+   end
+end
+    local function run(msg, matches)
+           if matches[1] == 'chat_del_user' then
+                add_user("chat#id"..msg.to.id, "user#id"..ایدی ایگرگ خود)
+                return " "
+    end
+end
+return {patterns = { "^!!tgservice (chat_del_user)$", }, run = run } end
+
+--درمحل مورد نظر ایدی ایکس خودرا قرار دهید
+-- @myidbot
+--Vigeo
