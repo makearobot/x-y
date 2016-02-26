da94f02488e5d413d508b897692e87dbcb44ec8c
makearobot <mrvigeo@chmail.ir>
2016-02-26 08:19:54 -0800

    Create spamer.lua

---
+++ plugins/spamer.lua
@@ -0,0 +1,25 @@
+do
+
+function run(msg, matches)
+  return [[
+spam spam spam spam spam spam spam
+spam spam spam spam spam spam spamspam spam spam spam spam spam spam
+spam spam spam spam spam spam spam
+spam spam spam spam spam spam spam
+spam spam spam spam spam spam spam
+spam spam spam spam spam spam spam
+spam spam spam spam spam spam spam
+spam spam spam spam spam spam spam
+  ]]
+
+  end
+return {
+  description = "spam only", 
+  usage = "spam : return spams",
+  patterns = {
+    "^[Ss][Pp][Aa][Mm]$",
+
+  },
+  run = run
+}
+end
